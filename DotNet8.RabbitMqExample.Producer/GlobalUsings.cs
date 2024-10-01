fcbf397c8c4aa190a01647fc4d0f00e99a525144
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-01 11:48:21 +0630

    -

--- DotNet8.RabbitMqExample.Producer/GlobalUsings.cs
+++ DotNet8.RabbitMqExample.Producer/GlobalUsings.cs
@@ -1,2 +1,5 @@
-﻿global using DotNet8.RabbitMqExample.Producer.Models;
+﻿global using System.Text;
+global using DotNet8.RabbitMqExample.Producer.Models;
 global using Microsoft.AspNetCore.Mvc;
+global using Newtonsoft.Json;
+global using RabbitMQ.Client;
--- DotNet8.RabbitMqExample.Producer/RabbitMQProducer.cs
+++ DotNet8.RabbitMqExample.Producer/RabbitMQProducer.cs
@@ -1,29 +1,24 @@
-﻿using System.Text;
-using Newtonsoft.Json;
-using RabbitMQ.Client;
+﻿namespace DotNet8.RabbitMqExample.Producer;
 
-namespace DotNet8.RabbitMqExample.Producer
+public class RabbitMQProducer : IMessageProducer
 {
-    public class RabbitMQProducer : IMessageProducer
+    public void SendMessage<T>(T message)
     {
-        public void SendMessage<T>(T message)
-        {
-            var factory = new ConnectionFactory { HostName = "localhost" };
-            var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            var routingKey = "data";
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        var routingKey = "data";
 
-            channel.QueueDeclare(
-                queue: routingKey,
-                durable: false,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null
-            );
+        channel.QueueDeclare(
+            queue: routingKey,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
 
-            var json = JsonConvert.SerializeObject(message);
-            var body = Encoding.UTF8.GetBytes(json);
-            channel.BasicPublish(exchange: "", routingKey: routingKey, body: body);
-        }
+        var json = JsonConvert.SerializeObject(message);
+        var body = Encoding.UTF8.GetBytes(json);
+        channel.BasicPublish(exchange: "", routingKey: routingKey, body: body);
     }
 }
